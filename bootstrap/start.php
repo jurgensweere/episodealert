1bcd30790a6e4c10122c5e27d447e3bce34f5381
Arno Bats <arnobats@gmail.com>
2014-08-11 17:16:28 +0200

    Add grunt tasks to the project #11 - Added sass compile - Added uglify - Added grunt watch / livereload

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('homestead'),
+	'local' => array('homestead', gethostname()),
+	'production' => array('www.episode-alert.com')
 
 ));
 
---
+++ public/Gruntfile.js
@@ -0,0 +1,67 @@
+module.exports = function(grunt) {
+
+// 1. All configuration goes here
+grunt.initConfig({
+	pkg: grunt.file.readJSON('package.json'),
+
+		// Configuration for the Sass task
+		sass: {
+			dist: {
+				options: {
+					style: 'compressed'
+				},
+
+				//TODO These files should be combined and minified etc.
+				files: {
+					'css/global.css': 'scss/global.scss',
+					'css/admin_global.css': 'scss/admin_global.scss',
+					'css/facebox.css': 'scss/facebox.scss',
+					'css/global-mobile.css': 'scss/global-mobile.scss',
+					'css/jquery.autocomplete.css': 'scss/jquery.autocomplete.scss',
+					'css/reset-fonts-grids.css': 'scss/reset-fonts-grids.scss',
+				}
+			}
+		},
+
+		uglify: {
+			options: {
+				banner: ''
+			},
+			target_1: {
+				// Add files here when we implemented angular
+				src: ['js/global.js'],
+				dest: 'js/global.min.js'
+			}
+		},
+
+		watch: {
+			sass: {
+				files: ['scss/*.scss'],
+				tasks: ['sass'],
+			},
+			uglify: {
+				files: ['js/*.js'],
+				tasks: ['uglify']
+			},
+			livereload: {
+				options: {
+					livereload: true
+				},
+				files: [
+					'../app/views/*.php',
+					'css/*.css',
+					'js/*.js'
+				]
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	//Default tasks to run when you type 'grunt'
+	grunt.registerTask('default', ['watch']);
+
+};
