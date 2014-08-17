6ff5f325f24c67ee029df2b28e4fe7e35db0a94d
castor385 <jurgen.sweere@gmail.com>
2014-08-17 15:24:56 +0200

    Clickable Carousel Items
    Very cool stuffz

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,37 @@
 (function(){
-	var app = angular.module('eaApp', []);
+    var app = angular.module('eaApp', []);
 
-	app.directive("carousel", function() {
-		return {
-			restrict: 'E',
-			templateUrl: 'carousel.html',
-			controller: ['$http', function($http) {
-				var carousel = this;
-				carousel.series = [];
+    app.directive("carousel", function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'carousel.html',
+            controller: ['$http', function($http) {
+                var carousel = this;
+                carousel.series = [];
+                carousel.currentSeries = 0;
+                carousel.backgroundStyle = {};
 
-				$http.get('/api/series/top').success(function(data) {
-					carousel.series = data;
-				});
-			}],
-			controllerAs: 'carousel'
-		};
-	});
+                $http.get('/api/series/top').success(function(data) {
+                    carousel.series = data;
+                    if (data[0] != undefined) {
+                        carousel.selectSeries(data[0].id);
+                    }
+                });
+
+                carousel.selectSeries = function(seriesId) {
+                    carousel.currentSeries = seriesId;
+
+                    for (var i = carousel.series.length - 1; i >= 0; i--) {
+                        if (carousel.series[i].id == carousel.currentSeries) {
+                            carousel.backgroundStyle = {'background-image': 'url(../img/fanart/' + carousel.series[i].fanart_image +')'};
+                        }
+                    };
+                }
+                carousel.isSelected = function(seriesId) {
+                    return seriesId == carousel.currentSeries;
+                }
+            }],
+            controllerAs: 'carousel'
+        };
+    });
 })();
