fa59ad055a6802a3411db03eefa845b5676034b8
rekhav <dishita.rekha@gmail.com>
2014-09-09 23:30:43 +0200

    Added a new category adn camel cased genre

--- public/js/controllers/seriesListController.js
+++ public/js/controllers/seriesListController.js
@@ -3,19 +3,22 @@
     angular.module('eaApp').controller('SeriesListCtrl',
         function($scope, $routeParams, seriesFactory) {
         	var genre = $routeParams.genre;
-        	$scope.genre = genre;
+        	$scope.genre = toTitleCase(genre);
         	 getByGenre(genre);
 
           function getByGenre(genre) {
             seriesFactory.getByGenre(genre)
-            .success(function (series) {
-            	console.log(series);            	
+            .success(function (series) {            	        	
               $scope.series = series;
             })
             .error(function (error) {
               //$scope.status = 'error error error beep beep;
             });
           }
+
+          function toTitleCase(str) {
+		    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+		  }
         }
     );
 
