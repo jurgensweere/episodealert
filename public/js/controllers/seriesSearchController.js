46d48e2dc9f5856ae6b5502a980b1ec4f2db1495
castor385 <jurgen.sweere@gmail.com>
2015-02-11 13:31:30 +0100

    Fixed empty search
    only empty searches now show: "Nothing found, ...."

--- public/js/controllers/seriesSearchController.js
+++ public/js/controllers/seriesSearchController.js
@@ -2,7 +2,6 @@
     angular.module('eaApp').controller('SeriesSearchCtrl', function($scope, $location, seriesFactory) {
 
         $scope.mainPageQuery = '';
-        $scope.results = 0;
 
         //There is some debouncing (500ms delay to wait for the user to stop typing) in the HTML, this will start to work with angular 1.3 it seems
         // Start watching the search box for input
