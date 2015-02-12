793ace10081a621a233136bb3638ab35bcf85cea
Jurgen Sweere <jurgen.sweere@gmail.com>
2015-02-12 15:15:33 +0100

    Show default page title
    fixes #43

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>{{ Page.getTitle() }}</title>
+    <title ng-bind="Page.getTitle()">Episode-Alert</title>
     <base href="/"/>
 
     <% HTML::style('//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css') %>
