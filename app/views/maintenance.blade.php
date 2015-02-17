6b761f7c8262e159ef80af98eb3b54f15177d7f9
castor385 <jurgen.sweere@gmail.com>
2015-02-17 16:35:37 +0100

    Maintenance page

---
+++ app/views/maintenance.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    
+    <meta name="robots" content="index,follow">
+    <meta name="keywords" content="tv, series, episode, alert, Reality TV Shows, Comedy TV Shows, Old Television Shows, Reality TV, Comedy TV, TV Shows, Television Shows, Old TV Shows, Action/Adventure, Animation, Children, Comedy, Drama, Science-Fiction, Soap, Talk Shows, Popular Shows, TV Listings, CBS, NBC, Fox, HBO, ABC, CW" />
+    <meta name="description" content="The best source for show and episode info. Keeping you up to date on the latest broadcasts" />  
+
+    <title>Episode-Alert</title>
+    <base href="/"/>
+
+    <% HTML::style('//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css') %>
+    <% HTML::style(asset('css/global.css')) %>
+
+</head>
+<body>
+	We are upgrading the website!
+</body>
+</html>
