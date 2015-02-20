8ff6a51992368f92db68b2a2ad618bc2f4ccd21f
castor385 <jurgen.sweere@gmail.com>
2015-02-20 16:23:46 +0100

    Migration fix
    Fixed incorrect DB statement in migration file

--- app/database/migrations/2015_02_19_130440_create_foreign_keys.php
+++ app/database/migrations/2015_02_19_130440_create_foreign_keys.php
@@ -17,15 +17,15 @@ class CreateForeignKeys extends Migration {
         // There are some following records that have no user record. Delete those
         DB::statement('delete following from following left join user u on u.id = following.user_id where u.id is null;');
         DB::statement('ALTER TABLE `following` ADD CONSTRAINT `following_series_id_foreign` FOREIGN KEY (`series_id`) REFERENCES `series` (`id`) ON DELETE CASCADE,
-                                                   CONSTRAINT `following_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;');
+                                               ADD CONSTRAINT `following_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;');
         
         // There are some seen records that have no episodes
         DB::statement('delete seen from seen left join episode e on e.id = seen.episode_id where e.id is null;');
         DB::statement('delete seen from seen left join series s on s.id = seen.series_id where s.id is null;');
         DB::statement('delete seen from seen left join user u on u.id = seen.user_id where u.id is null;');
         DB::statement('ALTER TABLE `seen` ADD CONSTRAINT `seen_episode_id_foreign` FOREIGN KEY (`episode_id`) REFERENCES `episode` (`id`) ON DELETE CASCADE,
-                                              CONSTRAINT `seen_series_id_foreign` FOREIGN KEY (`series_id`) REFERENCES `series` (`id`) ON DELETE CASCADE,
-                                              CONSTRAINT `seen_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;');
+                                          ADD CONSTRAINT `seen_series_id_foreign` FOREIGN KEY (`series_id`) REFERENCES `series` (`id`) ON DELETE CASCADE,
+                                          ADD CONSTRAINT `seen_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;');
         DB::statement('ALTER TABLE `seen` ADD INDEX `index_episode_n` (`episode`);');
         DB::statement('ALTER TABLE `seen` ADD INDEX `index_season_n` (`season`);');
 
