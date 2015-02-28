0aa54ee833272b2056b61349dca2a252f039902c
castor385 <jurgen.sweere@gmail.com>
2015-02-28 15:26:34 +0100

    Gulp + Environment tasks

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -12,60 +12,147 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
 var paths = {
-  js: ['./js/**/*.js', '!./js/vendor/**/*.js'],
+  js: ['./js/**/*.js', '!./js/vendor/**/*.js', '!./js/**/*.min.js'],
   scss: './scss/global.scss',
   css: ['./css/*.css', '!./css/*.min.css']
 };
 
-gulp.task('default', ['watch', 'sass', 'jshint']);
+var environments = ['dev', 'beta', 'live'];
 
-gulp.task('sass', function() {
+//Define runnable tasks
+environments.forEach(function (environment) {
+    gulp.task('default:' + environment, 
+        ['watch:' + environment,
+         'sass:' + environment,
+         'jshint:' + environment]
+    );
+
+    gulp.task('sass:' + environment, function() {
+        sassTask(environment);
+    });
+
+    gulp.task('jshint:' + environment, function() {
+        jshintTask(environment);
+    });
+
+    gulp.task('watch:' + environment, function() {
+        watchTask(environment);
+    });
+
+    gulp.task('build:' + environment,
+        ['sass:' + environment,
+         'jshint:' + environment]
+        , function () {
+            buildCssTask(environment);
+            bowerConcatTask(environment);
+            buildJsTask(environment);
+        }
+    );
+});
+// Define default fallback
+gulp.task('default', ['default:dev']);
+gulp.task('sass', ['sass:dev']);
+gulp.task('jshint', ['jshint:dev']);
+gulp.task('build', ['build:dev']);
+gulp.task('watch', ['watch:dev']);
+
+// Helper functions
+var watchTask = function (env) {
+    livereload.listen();
+    gulp.watch(paths.js, ['jshint:' + env]);
+    gulp.watch(paths.scss, ['sass:' + env]);
+}
+
+var sassTask = function (env) {
     gulp.src(paths.scss)
         .pipe(sass())
         .pipe(gulp.dest('./css'))
         .pipe(livereload());
-});
+};
 
-gulp.task('jshint', function() {
+var jshintTask = function(env) {
     gulp.src(paths.js)
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'))
         .pipe(livereload());
-});
+};
 
-function bowerConcat() {
+var bowerConcatTask = function(env) {
     var jsFilter = gulpFilter('*.js');
     var cssFilter = gulpFilter('*.css');
     var fontFilter = gulpFilter(['*.eot', '*.woff', '*.woff2', '*.svg', '*.ttf']);
+
+// TODO: simplify these paths, only change the base path.
+    var path = {
+        'js': {
+            'dev': './js/vendor',
+            'beta': './dist/beta/js/vendor',
+            'live': './dist/production/js/vendor',
+        },
+        'css': {
+            'dev': './css/vendor',
+            'beta': './dist/beta/css/vendor',
+            'live': './dist/production/css/vendor',
+        },
+        'font': {
+            'dev': './fonts/bootstrap',
+            'beta': './dist/beta/fonts/bootstrap',
+            'live': './dist/production/fonts/bootstrap',
+        },
+    }
+
+    // TODO: On live, we also want to copy these files, maybe this needs to be a seperate function?
+    // cwd: 'templates',
+    // src: ['**'],
+    // dest: 'dist/production/templates/'
+
+    // cwd: 'js/vendor/',
+    // src: ['ui-bootstrap*.js'],
+    // dest: 'dist/production/js/vendor/'
 
     gulp.src(mainBowerFiles())
         // Concat all js files
         .pipe(jsFilter)
         .pipe(concat('_bower.js'))
-        .pipe(gulp.dest('./js/vendor'))
+        .pipe(gulp.dest(path.js[env]))
         .pipe(jsFilter.restore())
 
         // Concat all css files
         .pipe(cssFilter)
         .pipe(concat('_bower.css'))
-        .pipe(gulp.dest('./css/vendor'))
+        .pipe(gulp.dest(path.css[env]))
         .pipe(cssFilter.restore())
 
         // Copy fonts
         .pipe(fontFilter)
-        .pipe(gulp.dest('./fonts/bootstrap')); // <-- check the path, or change it?
-
+        .pipe(gulp.dest(path.font[env])); // <-- check the path, or change it?
 };
 
-gulp.task('build', ['jshint', 'sass'], function () {
+var buildCssTask = function (env) {
+    var path = {
+        'dev': './dist/local/css',
+        'beta': './dist/beta/css',
+        'live': './dist/production/css',
+    }
+
     gulp.src(paths.css)
         .pipe(minifyCSS({ keepBreaks: true }))
         .pipe(rename({
             suffix: '.min'
         }))
-        .pipe(gulp.dest('./dist/local/css'));
+        .pipe(gulp.dest(path[env]));
+};
 
-    bowerConcat();
+var buildJsTask = function (env) {
+    var path = {
+        'dev': './dist/js',
+        'beta': './dist/beta/js',
+        'live': './dist/production/js',
+    }
+
+    //TODO: On live, we also want to uglify these:
+    //'dist/production/js/vendor/_bower.min.js': 'dist/production/js/vendor/_bower.js',
+    //'dist/production/js/vendor/ui-bootstrap-0.12.0.min.js': 'dist/production/js/vendor/ui-bootstrap-0.12.0.js',
 
     gulp.src(paths.js)
         .pipe(concat('ea.js'))
@@ -73,12 +160,5 @@ gulp.task('build', ['jshint', 'sass'], function () {
         .pipe(rename({
             suffix: '.min'
         }))
-        .pipe(gulp.dest('./dist/js'));
-});
-
-gulp.task('watch', function() {
-    livereload.listen();
-    gulp.watch(paths.js, ['jshint']);
-    gulp.watch(paths.scss, ['sass']);
-});
-
+        .pipe(gulp.dest(path[env]));
+};
