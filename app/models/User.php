b1848ec756a19bf887c4b3c17a5fa46cbb728f9e
Maarten Staa <aistina@gmail.com>
2015-03-06 11:44:53 +0100

    Fix PHP 5.4 compatibility

--- app/models/User.php
+++ app/models/User.php
@@ -6,92 +6,92 @@ use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
 use Eloquent;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
+class User extends Eloquent implements UserInterface, RemindableInterface
+{
+    use UserTrait, RemindableTrait;
 
-	use UserTrait, RemindableTrait;
+    const ROLE_MEMBER = 'member';
 
-	const ROLE_MEMBER = 'member';
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'user';
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'user';
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = array('password');
 
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = array('password');
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
 
-	/**
-	 * Indicates if the model should be timestamped.
-	 *
-	 * @var bool
-	 */
-	public $timestamps = true;
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = array();
 
-	/**
-	 * The attributes that aren't mass assignable.
-	 *
-	 * @var array
-	 */
-	protected $guarded = array();
+    /**
+     * Get the unique identifier for the user.
+     *
+     * @return mixed
+     */
+    public function getAuthIdentifier()
+    {
+        return $this->getKey();
+    }
 
-	/**
-	 * Get the unique identifier for the user.
-	 *
-	 * @return mixed
-	 */
-	public function getAuthIdentifier()
-	{
-		return $this->getKey();
-	}
+    /**
+     * Get the password for the user.
+     *
+     * @return string
+     */
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
 
-	/**
-	 * Get the password for the user.
-	 *
-	 * @return string
-	 */
-	public function getAuthPassword()
-	{
-		return $this->password;
-	}
+    /**
+     * Get the e-mail address where password reminders are sent.
+     *
+     * @return string
+     */
+    public function getReminderEmail()
+    {
+        return $this->email;
+    }
 
-	/**
-	 * Get the e-mail address where password reminders are sent.
-	 *
-	 * @return string
-	 */
-	public function getReminderEmail()
-	{
-		return $this->email;
-	}
+    public function getRememberToken()
+    {
+        return $this->remember_token;
+    }
 
-	public function getRememberToken()
-	{
-		return $this->remember_token;
-	}
+    public function setRememberToken($value)
+    {
+        $this->remember_token = $value;
+    }
 
-	public function setRememberToken($value)
-	{
-		$this->remember_token = $value;
-	}
+    public function getRememberTokenName()
+    {
+        return 'remember_token';
+    }
 
-	public function getRememberTokenName()
-	{
-		return 'remember_token';
-	}
+    public function isThirdParty()
+    {
+        return $this->oauthprovider != null;
+    }
 
-	public function isThirdParty()
-	{
-		return $this->oauthprovider != null;
-	}
-
-	public function following()
-	{
-		return $this->belongsToMany(Series::class, 'following');
-	}
+    public function following()
+    {
+        return $this->belongsToMany('EA\models\Series', 'following');
+    }
 }
