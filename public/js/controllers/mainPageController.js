acebe91d3887d99baaae54d06a6181a7fff648ad
Maarten Staa <aistina@gmail.com>
2015-03-11 22:09:25 +0100

    Fix typo in filename

--- public/js/controllers/mainPageController.js
+++ public/js/controllers/mainPageController.js
@@ -2,12 +2,13 @@
 
 	/*
 	 * This controller is used for binding things that need to be available page wide
-	 */ 
+	 */
 
     angular.module('eaApp').controller('MainPageCtrl',
         function($scope, Page, alertService) {
             $scope.Page = Page;
         	$scope.alerts = alertService.get();
+        	$scope.context = '';
         }
     );
 })();
