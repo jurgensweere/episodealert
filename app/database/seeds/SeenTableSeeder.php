1394c1fe4b1e982d7a3774ce684e07b9270512d3
Jurgen Sweere <jurgen.sweere@gmail.com>
2015-04-01 15:46:54 +0200

    More coverage for SeriesController
    SeriesController needs some serious work to clean it up

--- app/database/seeds/SeenTableSeeder.php
+++ app/database/seeds/SeenTableSeeder.php
@@ -24,6 +24,48 @@ class SeenTableSeeder extends Seeder {
 				'updated_at' => '0000-00-00 00:00:00',
 			),
 		));
+        
+		\DB::table('seen')->insert(array (
+			0 => 
+			array (
+				'id' => 2,
+				'user_id' => 1,
+				'series_id' => 72449,
+				'episode_id' => 85774,
+				'season' => 2,
+				'episode' => 2,
+				'created_at' => '0000-00-00 00:00:00',
+				'updated_at' => '0000-00-00 00:00:00',
+			),
+		));
+        
+		\DB::table('seen')->insert(array (
+			0 => 
+			array (
+				'id' => 3,
+				'user_id' => 1,
+				'series_id' => 72449,
+				'episode_id' => 85775,
+				'season' => 2,
+				'episode' => 3,
+				'created_at' => '0000-00-00 00:00:00',
+				'updated_at' => '0000-00-00 00:00:00',
+			),
+		));
+        
+		\DB::table('seen')->insert(array (
+			0 => 
+			array (
+				'id' => 4,
+				'user_id' => 1,
+				'series_id' => 72449,
+				'episode_id' => 85776,
+				'season' => 2,
+				'episode' => 4,
+				'created_at' => '0000-00-00 00:00:00',
+				'updated_at' => '0000-00-00 00:00:00',
+			),
+		));
 	}
 
 }
--- app/tests/SeriesControllerTest.php
+++ app/tests/SeriesControllerTest.php
@@ -1,6 +1,7 @@
 <?php namespace EA\tests;
 
 use Route;
+use EA\models\Seen;
 
 class SeriesControllerTest extends TestCase
 {
@@ -220,6 +221,8 @@ class SeriesControllerTest extends TestCase
     {
         $this->beAdmin();
 
+        $initialSeenCount = Seen::all()->count();
+
         $crawler = $this->client->request('POST', '/api/series/seen/85753');
         $response = $this->client->getResponse();
         $data = $response->getData();
@@ -227,6 +230,238 @@ class SeriesControllerTest extends TestCase
         $this->assertTrue($response->isOk());
         $this->assertInstanceOf('\Illuminate\Http\JsonResponse', $response);
         
-        //TODO: figure out a way to check database contents
+        // One extra seen record created
+        $this->assertEquals($initialSeenCount + 1, Seen::all()->count());
     }
+
+    public function testSetSeenEpisodeSeasonMode()
+    {
+        $this->beAdmin();
+
+        $initialSeenCount = Seen::all()->count();
+
+        $crawler = $this->client->request('POST', '/api/series/seen/85753', array('mode' => Seen::MODE_SEASON));
+        $response = $this->client->getResponse();
+        $data = $response->getData();
+
+        $this->assertTrue($response->isOk());
+        $this->assertInstanceOf('\Illuminate\Http\JsonResponse', $response);
+        
+        // One extra seen record created
+        $this->assertEquals($initialSeenCount + 21, Seen::all()->count());
+    }
+
+    public function testSetSeenEpisodeUntilMode()
+    {
+        $this->beAdmin();
+
+        $initialSeenCount = Seen::all()->count();
+
+        $crawler = $this->client->request('POST', '/api/series/seen/85753', array('mode' => Seen::MODE_UNTIL));
+        $response = $this->client->getResponse();
+        $data = $response->getData();
+
+        $this->assertTrue($response->isOk());
+        $this->assertInstanceOf('\Illuminate\Http\JsonResponse', $response);
+        
+        // One extra seen record created
+        $this->assertEquals($initialSeenCount + 3, Seen::all()->count());
+    }
+
+    public function testSetSeenEpisodeUnknownMode()
+    {
+        $this->beAdmin();
+
+        $initialSeenCount = Seen::all()->count();
+
+        $crawler = $this->client->request('POST', '/api/series/seen/85753', array('mode' => 'worstenbrood'));
+        $response = $this->client->getResponse();
+        $data = $response->getData();
+
+        $this->assertFalse($response->isOk());
+        $this->assertInstanceOf('\Illuminate\Http\JsonResponse', $response);
+        
+        // One extra seen record created
+        $this->assertEquals($initialSeenCount, Seen::all()->count());
+    }
+
+    public function testSetUneenEpisodeNotLoggedIn()
+    {
+        Route::enableFilters();
+
+        $crawler = $this->client->request('POST', '/api/series/unseen/85775');
+        $response = $this->client->getResponse();
+
+        $this->assertFalse($response->isOk());
+    }
+
+    public function testSetUnseenEpisode()
+    {
+        $this->beAdmin();
+
+        $initialSeenCount = Seen::all()->count();
+
+        $crawler = $this->client->request('POST', '/api/series/unseen/85775');
+        $response = $this->client->getResponse();
+        $data = $response->getData();
+
+        $this->assertTrue($response->isOk());
+        $this->assertInstanceOf('\Illuminate\Http\JsonResponse', $response);
+        
+        // One extra seen record created
+        $this->assertEquals($initialSeenCount - 1, Seen::all()->count());
+    }
+
+    public function testSetUnseenEpisodeSeasonMode()
+    {
+        $this->beAdmin();
+
+        $initialSeenCount = Seen::all()->count();
+
+        $crawler = $this->client->request('POST', '/api/series/unseen/85775', array('mode' => Seen::MODE_SEASON));
+        $response = $this->client->getResponse();
+        $data = $response->getData();
+
+        $this->assertTrue($response->isOk());
+        $this->assertInstanceOf('\Illuminate\Http\JsonResponse', $response);
+        
+        // One extra seen record created
+        $this->assertEquals($initialSeenCount - 4, Seen::all()->count());
+    }
+
+    public function testSetUnseenEpisodeUntilMode()
+    {
+        $this->beAdmin();
+
+        $initialSeenCount = Seen::all()->count();
+
+        $crawler = $this->client->request('POST', '/api/series/unseen/85775', array('mode' => Seen::MODE_UNTIL));
+        $response = $this->client->getResponse();
+        $data = $response->getData();
+
+        $this->assertTrue($response->isOk());
+        $this->assertInstanceOf('\Illuminate\Http\JsonResponse', $response);
+        
+        // One extra seen record created
+        $this->assertEquals($initialSeenCount - 3, Seen::all()->count());
+    }
+
+    public function testSetUnseenEpisodeUnknownMode()
+    {
+        $this->beAdmin();
+
+        $initialSeenCount = Seen::all()->count();
+
+        $crawler = $this->client->request('POST', '/api/series/unseen/85775', array('mode' => 'worstenbrood'));
+        $response = $this->client->getResponse();
+        $data = $response->getData();
+
+        $this->assertFalse($response->isOk());
+        $this->assertInstanceOf('\Illuminate\Http\JsonResponse', $response);
+        
+        // One extra seen record created
+        $this->assertEquals($initialSeenCount, Seen::all()->count());
+    }
+
+    public function testGetUnseenEpisodesNotLoggedIn()
+    {
+        Route::enableFilters();
+
+        $crawler = $this->client->request('GET', '/api/series/unseenamount');
+        $response = $this->client->getResponse();
+
+        $this->assertFalse($response->isOk());
+    }
+
+    public function testGetUnseenEpisodes()
+    {
+        $this->beAdmin();
+
+        $crawler = $this->client->request('GET', '/api/series/unseenamount');
+        $response = $this->client->getResponse();
+        $data = $response->getData();
+
+        $this->assertTrue($response->isOk());
+        $this->assertInstanceOf('\Illuminate\Http\JsonResponse', $response);
+        
+        $this->assertObjectHasAttribute('unseenepisodes', $data);
+        $this->assertEquals(46, $data->unseenepisodes); // (53 - 3 not following - 4 seen)
+    }
+
+    public function testGetUnseenEpisodesPerSeasonNotLoggedIn()
+    {
+        Route::enableFilters();
+
+        $crawler = $this->client->request('GET', '/api/series/unseenamountbyseason/72449/2');
+        $response = $this->client->getResponse();
+
+        $this->assertFalse($response->isOk());
+    }
+
+    public function testGetUnseenEpisodesPerSeason()
+    {
+        $this->beAdmin();
+
+        $crawler = $this->client->request('GET', '/api/series/unseenamountbyseason/72449/2');
+        $response = $this->client->getResponse();
+        $data = $response->getData();
+
+        $this->assertTrue($response->isOk());
+        $this->assertInstanceOf('\Illuminate\Http\JsonResponse', $response);
+        
+        $this->assertObjectHasAttribute('unseenepisodes', $data);
+        $this->assertEquals(18, $data->unseenepisodes); // (22 - 4 seen)
+    }
+
+    public function testGetUnseenEpisodesPerSeriesNotLoggedIn()
+    {
+        Route::enableFilters();
+
+        $crawler = $this->client->request('GET', '/api/series/unseenamountbyseries/72449/3');
+        $response = $this->client->getResponse();
+
+        $this->assertFalse($response->isOk());
+    }
+
+    public function testGetUnseenEpisodesPerSeries()
+    {
+        $this->beAdmin();
+
+        $crawler = $this->client->request('GET', '/api/series/unseenamountbyseries/72449/3');
+        $response = $this->client->getResponse();
+        $data = $response->getData();
+
+        $this->assertTrue($response->isOk());
+        $this->assertInstanceOf('\Illuminate\Http\JsonResponse', $response);
+        
+        $this->assertCount(3, $data);
+        $this->assertEquals(21, $data[0]);
+        $this->assertEquals(18, $data[1]);
+        $this->assertEquals(7, $data[2]);
+    }
+
+    // TODO: FIX THIS FUNCTION
+    public function testGetEpisodesForUserPerDateNotLoggedIn()
+    {
+        Route::enableFilters();
+
+        $crawler = $this->client->request('GET', '/api/series/episodesperdate/1999-07-24');
+        $response = $this->client->getResponse();
+
+        $this->assertFalse($response->isOk());
+    }
+
+    // public function testGetEpisodesForUserPerDate()
+    // {
+    //     $this->beAdmin();
+
+    //     $crawler = $this->client->request('GET', '/api/series/episodesperdate/1999-07-23');
+    //     $response = $this->client->getResponse();
+    //     $data = $response->getData();
+
+    //     $this->assertTrue($response->isOk());
+    //     $this->assertInstanceOf('\Illuminate\Http\JsonResponse', $response);
+        
+    //     $this->assertCount(1, $data);
+    // }
 }
