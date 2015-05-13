bb715cf6cb670f4ceb81911a2a64e216098cef33
abats <arnobats@gmail.com>
2015-05-13 16:36:32 +0200

    Back to 5!

--- app/controllers/SeriesController.php
+++ app/controllers/SeriesController.php
@@ -79,7 +79,7 @@ class SeriesController extends BaseController
             )
             ->where('fanart_image_converted', '=', 1)
             ->get()
-            ->random(10);
+            ->random(5);
 
         $array = array();
         if (count($topSeries) > 0) {
